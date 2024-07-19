5abaa078dabe321f5ae8ff6e22d60602f143a57f
John Wilger <john@johnwilger.com>
2024-07-18 21:58:55 -0700

    Change unwrap/1 to only return the value
    When it returns a tagged tuple, it is much harder to use it inline. And
    there isn't much benefit to having it return a tagged tuple, because if
    the argument is the correct type, then it will unwrap successfully, and
    if it's not, the unwrap function will raise a FunctionClauseError.
    Dialyzer should also complain if you try to pass in the wrong type.

--- test/support/protocols.ex
+++ test/support/protocols.ex
@@ -6,7 +6,7 @@ end
 
 Newt.ExampleIntegerType.defimpl Add do
   def add_99(value) do
-    {:ok, number} = Newt.ExampleIntegerType.unwrap(value)
+    number = Newt.ExampleIntegerType.unwrap(value)
     number + 99
   end
 end
