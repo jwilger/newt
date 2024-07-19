5f62ba475c1b2433a058a8bdfe269cf6940fc739
John Wilger <john@johnwilger.com>
2024-07-19 12:35:11 -0700

    Bump version to 2.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Newt.MixProject do
   def project do
     [
       app: :newt,
-      version: "2.0.0",
+      version: "2.1.0",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       aliases: aliases(),
