b81c84cc1abb1b7a7c57d39df991f15187633127
John Wilger <john@johnwilger.com>
2024-08-17 20:50:43 -0700

    Cast scalar values to type in Ectotype
    This is necessary to allow the newtype to be used in embedded schemas.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Newt.MixProject do
   def project do
     [
       app: :newt,
-      version: "4.3.0",
+      version: "5.0.0",
       elixir: "~> 1.17",
       elixirc_paths: elixirc_paths(Mix.env()),
       aliases: aliases(),
