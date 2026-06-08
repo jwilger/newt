445bdb91bb0d457b13e652a45f0aec0dc7c8fbc8
John Wilger <john@johnwilger.com>
2026-06-08 16:33:46 -0700

    chore: release 10.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Newt.MixProject do
   def project do
     [
       app: :newt,
-      version: "9.0.0",
+      version: "10.0.0",
       elixir: "~> 1.20",
       elixirc_paths: elixirc_paths(Mix.env()),
       aliases: aliases(),
